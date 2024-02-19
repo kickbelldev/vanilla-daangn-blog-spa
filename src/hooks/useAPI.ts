3d26e59b49513e176def2dc209f05652e7b4a98f
kickbelldev <kickbelldev@gmail.com>
2024-02-19 20:57:08 +0900

    feat: useAPI 훅 구현

---
+++ src/hooks/useAPI.ts
@@ -0,0 +1,31 @@
+import { HttpMethods, instance } from '@/libs/axios'
+import { useEffect, useState } from '@/libs/vtu/valueToUI'
+import { AxiosRequestConfig } from 'axios'
+
+export const useAPI = <T>(
+  method: HttpMethods,
+  url: string,
+  options?: AxiosRequestConfig,
+) => {
+  const [data, setData] = useState<T>()
+  const [isLoading, setIsLoading] = useState(true)
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  useEffect(() => {
+    try {
+      setIsLoading(true)
+      instance<T>({ method, url, ...options }).then((response) => {
+        if (response.status >= 400) {
+          return
+        }
+        setData(response.data)
+        setIsSuccess(true)
+        setIsLoading(false)
+      })
+    } catch {
+      setIsLoading(false)
+    }
+  }, [url, options])
+
+  return { data, isLoading, isSuccess }
+}
