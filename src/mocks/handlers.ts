f7f2e7a9f3aa02ff2f2cba47cf6b063820c77cd7
Jongchan <kickbelldev@gmail.com>
2024-02-20 15:30:42 +0900

    feat: msw 글상세 핸들러 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw'
-import { ARTICLE_LIST } from './data/articles'
+import { ARTICLE_DETAIL, ARTICLE_LIST } from './data/articles'
 
 export const handlers = [
   http.get('/api/user/:userId', ({ params }) => {
@@ -12,6 +12,11 @@ export const handlers = [
   }),
   http.get('/api/article', () => {
     return HttpResponse.json(ARTICLE_LIST)
+  }),
+  http.get('/api/article/:id', ({ params }) => {
+    const { id } = params
+    const article = ARTICLE_DETAIL.find((article) => article.id === Number(id))
+    return HttpResponse.json(article)
   }),
   // http.all('*', () => {
   //   return HttpResponse.error()
