9af431a84e8bb7413b1f2de7396568d5bb2cedb8
Jongchan <kickbelldev@gmail.com>
2024-02-23 16:15:00 +0900

    typing: jsx 타이핑 개선

--- src/libs/jsx/jsx-runtime.ts
+++ src/libs/jsx/jsx-runtime.ts
@@ -1,24 +1,36 @@
+import { VirtualDOMNode } from '../vtu/createDOM'
+
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-namespace */
 declare global {
   module JSX {
     type IntrinsicElements = {
-      [elemName in keyof HTMLElementTagNameMap]: Record<string, any>
+      [elemName in keyof HTMLElementTagNameMap]: Record<string, unknown>
+    }
+    interface Element {
+      tag: keyof HTMLElementTagNameMap
+      props: Record<string, unknown>
+      children: VirtualDOMNode[]
     }
   }
 }
 
-export type Component = (props?: Record<string, any>) => any
+export interface DefaultProps {
+  children?: VirtualDOMNode[]
+}
+
+export type Component<T extends DefaultProps = DefaultProps> = (
+  props: T,
+) => JSX.Element
 
 export const jsx = {
-  component(
+  toVDOM(
     component: string | Component,
-    props: Record<string, any> | null,
-    ...children: any[]
+    props: Record<string, unknown> | null,
+    ...children: VirtualDOMNode[]
   ) {
     if (typeof component === 'function') {
       return component({ ...props, children })
     }
-
     return {
       tag: component,
       props,
